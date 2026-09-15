57303a198ea30c38a9abf10793fbabb32d6aceeb
JackAltman <cs2dtzq@163.com>
2026-09-15 12:08:50 +0800

    Patches: susfs_inline_hook_patches: update version to 2.3.00
    Change-Id: I0807667e61bb8363e2e72ed64c714bbbe1f83fec
    Signed-off-by: JackAltman <cs2dtzq@163.com>

--- Patches/susfs_inline_hook_patches.sh
+++ Patches/susfs_inline_hook_patches.sh
@@ -5,7 +5,7 @@
 # Tested kernel versions: 5.4, 4.19, 4.14, 4.9, 4.4, 3.18
 # 20251120
 
-# This Hook is only available for SuSFS v2.1.00 onwards.
+# This Hook is only available for SuSFS v2.3.00 onwards.
 
 patch_files=(
     fs/exec.c
